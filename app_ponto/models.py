6222bb714bb9047b72052b72638f0b4ee2c1b1af
Laboratórios de Informática <labins@labin.canoas>
2019-05-10 21:17:33 -0300

    migrate

--- app_ponto/models.py
+++ app_ponto/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Justificativa(models.Model):
-    pass
-
-
 
 class Config_horario(models.Model):
     horario_entrada_1=models.TimeField(blank=True, null=True)
@@ -28,24 +24,29 @@ class Funcionario(models.Model):
     telefone_celular=models.CharField("Telefone Celular", max_length=15, help_text='Numero de telefone celular no formato (99)99999-9999', null=True, blank=True)
     email=models.EmailField('E-mail',null=True, blank=True)
     cargo=models.ForeignKey(Cargo,on_delete=models.SET_NULL, blank=True, null=True)
-    supervisor = models.ForeignKey('Funcionario', on_delete=SET_NULL, blank=True, null=True)
-    config_horario=models.ForeignKey(Config_horario)
+    supervisor = models.ForeignKey('Funcionario', on_delete=models.SET_NULL, blank=True, null=True)
+    config_horario=models.ForeignKey(Config_horario,on_delete=models.SET_NULL, blank=True, null=True)
 
+
+    def __str__ (self):
+        return self.nome
+
+class status(models.Model):
+    nome=models.CharField(max_length=30)
 
     def __str__ (self):
         return self.nome
 
 
 class Frequencia(models.Model):
-    data=models.DateTimeField()
-    #justificativa=
+    data=models.DateField()
     horario_entrada_1=models.TimeField(null=True)
     horario_saida_1=models.TimeField(null=True)
     horario_entrada_2=models.TimeField(null=True)
     horario_saida_2=models.TimeField(null=True)
+    justificativa=models.TextField(null=True,blank=True)
 
     def set_horario_entrada(self, horario):
-
         self.horario_entrada_1 = horario
 
 
